8d2a734a4da69245fd95c6ee02c5ea32f2d2abc8
ngothanhthien <mn.bf.thien@gmail.com>
2022-05-31 05:19:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/spirit_island_random_setup',
   plugins: [vue()],
   resolve: {
     alias: {
